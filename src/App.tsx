1426a50ceef55b1a81d2bb134647abf9df4309a7
Romberto <korhoff@mail.ru>
2025-01-23 13:06:53 +0400

    Подключил глобальные стили и шрифты

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
 
 
   return (
-    <>
-      <h3>Здесь будут компоненты!!!</h3>
-    </>
+    <div className="container">
+      <h3>Здесь будут компоненты</h3>
+    </div>
   )
 }
 
