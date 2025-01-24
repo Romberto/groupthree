af371f3c138f7a0d800ad859382eb519b11ae80e
Romberto <korhoff@mail.ru>
2025-01-24 17:34:13 +0400

    компонент Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Button from "./components/UI/Button/Button";
+
 function App() {
   return (
     <div className="container">
       <h3>Здесь будут компоненты</h3>
+      <Button text="Join us now" mode="btn-white"/>
+      <Button text="Request demo" mode="btn-violet"/>
+      <Button text="Join us now" mode="btn-round-rigth"/>
     </div>
   );
 }
---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { ButtonHTMLAttributes } from "react";
+import styles from "../Button/Button.module.css";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  mode: "btn-violet" | "btn-white" | "btn-round-rigth";
+}
+
+const Button = (props: ButtonProps, { ...rest }) => {
+  return (
+    <button
+      {...rest}
+      className={`${
+        props.mode === "btn-violet"
+         ? styles.btn_violet
+         : props.mode === "btn-white"
+         ? styles.btn_white
+         : styles.btn_round_right
+      }`}
+    >
+      {props.text}
+    </button>
+  );
+};
+
+export default Button;
