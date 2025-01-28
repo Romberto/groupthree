61fa573bb1a9c271a87a90e311925367fcbbb9a9
Artem Sobolevskiy <sobolevskiy.a2010@mail.ru>
2025-01-28 21:51:10 +0300

    fix: buttons text and nav-list

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,28 +1,41 @@
+import React from "react";
 import styles from './Header.module.css'
 import Button from "../Button/Button"
 import logo from '../../../assets/logo.svg'
 import arrow from '../../../assets/arrow.svg'
 import help from '../../../assets/help.svg'
 
-export const Header = () => {
+const headerNavList: string[] = ['home', 'search', 'favorites']
+
+export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <nav className={styles.header_nav}>
-        <img src={logo} />
+        <a href="#"><img src={logo} /></a>
         <ul className={styles.nav_links}>
-          <li><a className={styles.nav_link} href="#">home</a></li>
-          <li><a className={styles.nav_link} href="#">search</a></li>
-          <li><a className={styles.nav_link} href="#">favorites</a></li>
+          {
+            headerNavList.map((item, index) => (
+              <li key={index}>
+                <a className={styles.nav_link} href="#">
+                  {item}
+                </a>
+              </li>
+            )) 
+          }
         </ul>
       </nav>
       <div className={styles.header_buttons}>
-        <Button mode='btn-white'>
-          <img src={arrow} />
-          Sign Up
+        <Button className={styles.header_button} mode='btn-white'>
+          <p className={styles.button_text}>
+            <img src={arrow} alt='arrow'/>
+            Sign Up
+          </p>
         </Button>
-        <Button mode='btn-violet'>
-          <img src={help} />
-          Help
+        <Button className={styles.header_button} mode='btn-violet'>
+          <p className={styles.button_text}>
+            <img src={help} alt='help' />
+            Help
+          </p>
         </Button>
       </div>
     </header>
