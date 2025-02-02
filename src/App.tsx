638d460eb8a9a1c7f61b7913148f535bb57e70f3
Artem Sobolevskiy <sobolevskiy.a2010@mail.ru>
2025-02-02 12:26:08 +0300

    feat: add enum with paths

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { HomePage } from "./components/pages/HomePage/HomePage.tsx";
 import { SearchPage } from "./components/pages/SearchPage/SearchPage.tsx";
 import { FavoritesPage } from "./components/pages/FavoritesPage/FavoritesPage.tsx";
 import { HistoryPage } from "./components/pages/HistoryPage/HistoryPage.tsx";
+import { PATH } from "./utils/constants.ts";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-      <Route path="/history" element={<HistoryPage />} />
-      <Route path="/sign-in" />
-      <Route path="/sign-up" />
+      <Route path={PATH.HOME} element={<HomePage />} />
+      <Route path={PATH.SEARCH} element={<SearchPage />} />
+      <Route path={PATH.FAVORITES} element={<FavoritesPage />} />
+      <Route path={PATH.HISTORY} element={<HistoryPage />} />
+      <Route path={PATH.SIGNIN} />
+      <Route path={PATH.SIGNUP} />
     </Routes>
   );
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export enum PATH {
+  HOME = '/',
+  SEARCH = '/search',
+  FAVORITES = '/favorites',
+  HISTORY = '/hitory',
+  SIGNIN = '/sign-in',
+  SIGNUP = '/sign-up'
+}
