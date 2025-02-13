0782c10d8fb3fa6330dfa0d7739b9651b24eae80
Artem Sobolevskiy <sobolevskiy.a2010@mail.ru>
2025-02-13 16:07:38 +0300

    feat: add redirect to SearchPage

--- src/components/pages/ArtworkPage/ArtworkPage.tsx
+++ src/components/pages/ArtworkPage/ArtworkPage.tsx
@@ -1,15 +1,16 @@
-import { BASE_IMAGE_URL, DEFAULTIMAGE } from '@/utils/constants';
 import styles from './ArtworkPage.module.css';
 import { fetchApiGetById } from '@/utils/api';
+import arrow from '@/assets/details-arrow.svg';
+import { BASE_IMAGE_URL, DEFAULTIMAGE, PATH } from '@/utils/constants';
 import { fetchArtworkDataType } from '@/utils/types';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router';
 
 export const ArtworkPage = () => {
   const { id } = useParams();
   const [artwork, setArtwork] = useState<fetchArtworkDataType | null>(null);
   const imageUrl = artwork?.image_id
-    ? `${BASE_IMAGE_URL}/${artwork?.image_id}/full/400,/0/default.jpg`
+    ? `${BASE_IMAGE_URL}/${artwork?.image_id}/full/500,/0/default.jpg`
     : DEFAULTIMAGE;
 
   useEffect(() => {
@@ -21,6 +22,10 @@ export const ArtworkPage = () => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
+        <Link to={PATH.SEARCH} className={styles.redirect}>
+          <img className={styles.redirect_arrow} src={arrow} />
+          Return to Search Page
+        </Link>
         <h2 className={styles.title}>{artwork?.title}</h2>
         <p className={styles.subtitle}>{artwork?.artist_display}</p>
       </header>
