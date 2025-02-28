13a796e7cf3a2851eb77889b24f94e31d502623e
Romberto <korhoff@mail.ru>
2025-02-28 12:09:32 +0400

    add: base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base:"/groupthree/",
   resolve: {
     alias: {
       '@': '/src', // Алиас для папки src
